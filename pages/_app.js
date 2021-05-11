226ae007fb8631e4dfc005af970d9aceb05e2474
Oscar Villanueva <javiondo68@gmail.com>
2021-05-11 13:31:07 -0500

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "tailwindcss/tailwind.css"
 
-function MyApp({ Component, pageProps }) {
+function Init({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Init
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return ( 
+    <h1>usando tscript</h1>
+  );
+}
+ 
+export default Home;
