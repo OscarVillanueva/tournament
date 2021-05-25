797b27d8d5034d466846fd5ffd2880f62c1600f7
Oscar Villanueva <javiondo68@gmail.com>
2021-05-25 13:36:36 -0500

    world schedule done

--- context/world/WorldState.tsx
+++ context/world/WorldState.tsx
@@ -142,12 +142,11 @@ const WorldState: FC = ({ children }) => {
                 victories: 0
             })
 
-
         const response = generateMatches( shuffledRanking )
 
-        const schedule = roundsSeparation( response )
-
-        setIntoStorage( "matches", schedule )
+        const schedule = roundsSeparation( response, shuffledRanking )
+        
+        // setIntoStorage( "matches", schedule )
 
         dispatch({
             type: SET_MATCHES,
@@ -183,68 +182,122 @@ const WorldState: FC = ({ children }) => {
 
     }
 
-    const roundsSeparation = ( matches: Array<any> ) : Match[] => {
+    const roundsSeparation = ( matches: Array<Match>, ranking: Array<Player> ) : Match[] => {
         
-        let schedule = {}
-        let draws: Match[] = []
-        const matchesPerRound = state.ranking.length / 2
-        const rounds = state.ranking.length - 1
+        const schedule = {}
+        const byes = []
+        const matchesPerRound = ranking.length / 2
+        const rounds = ranking.length - 1
 
-        for (let index = 0; index <= matchesPerRound; index++) 
-            schedule[ `round-${ index + 1 }` ] = []
+        ranking.shift()
 
-        while( matches.length > 0 ) {
+        for (let index = 0; index < rounds; index++) 
+            schedule[ `round-${ index + 1 }` ] = [ {round: `Ronda ${ index + 1 }` ,...matches.shift()} ]
 
-            const currentGame = matches.shift()
-            let flag = true
-            let counter = 1
+        for (let index = 1; index <= rounds; index++) {
+            
+            let games = 1
+            let added = true
+            let current: Player = null
+            let byesIterator = byes[Symbol.iterator]()
+            let rankingIterator = ranking[ Symbol.iterator ]()
 
-            while ( flag && counter <= rounds) {
+            while( games < matchesPerRound ) {
 
-                if( lookFor( schedule[ `round-${ counter }` ], currentGame ) ) {
+                if( added ) current = next( schedule[`round-${ index }`], byesIterator, rankingIterator )
 
-                    schedule[ `round-${ counter }` ].push({ 
-                        ...currentGame,
-                        round: `Ronda ${ counter }`
-                    }) 
-                    flag = false
+                let match = matches.findIndex( 
+                    m => m.home.id === current.id || m.visitor.id === current.id 
+                )
+
+                if( lookFor( schedule[`round-${ index }`], matches[ match ], matchesPerRound) ) {
+
+                    schedule[ `round-${ index }` ].push({ 
+                        ...matches[ match ],
+                        round: `Ronda ${ index }`
+                    })
+
+                    if( matches[ match ].visitor.name === "BYE" ) 
+                        byes.push( matches[ match ].home )
+                        
+
+                    matches.splice( match, 1 )
+
+                    games = games + 1
+                    added = true
+                    
 
                 }
+                else {
 
-                counter = counter + 1
+                    let temp = matches[ match ]
+                    matches.push( temp )
+
+                    matches.splice( match, 1 )
+                    
+                    added = false
+
+                }
 
             }
 
         }
 
-        const result= Object.entries( schedule ).map( i => i[1] ) 
+        const result = Object.entries( schedule ).map( i => i[1] ) 
 
         return result.flat(1) as Match[]
 
     }
 
-    const lookFor = ( games: Match[], match: Match ) : boolean => {
+    const next = ( 
+        matches: Array<Match>, 
+        byes: IterableIterator<Player>, players: IterableIterator<Player> ) : Player => {
         
-        if( games.length === 0 ) return true
-
         let flag = true
-        let counter = 0
+        let search: any = { value: null, done: false }
 
-        while( flag && counter < games.length) {
+        while ( flag && !search.done ) {
 
-            let currentGame = games[ counter ]
+            search = byes.next()
 
-            if( currentGame.home.id === match.home.id  || currentGame.visitor.id === match.home.id )
-                flag = false
+            if( search.done ) search = players.next()
 
-            if( currentGame.home.id === match.visitor.id  || currentGame.visitor.id === match.visitor.id )
-                flag = false
+            if( !search.done ) {
 
-            counter = counter + 1
+                const isInMatches = matches.find( m => {
+
+                    if( m.home.id === search.value.id || m.visitor.id === search.value.id)
+                        return m
+
+                })
+                    
+                flag = isInMatches ? true : false
+            }
+
 
         }
 
-        return flag
+        return search.value
+
+    }
+
+    const lookFor = ( games: Match[], match: Match, matchesPerRound: number ) : boolean => {
+        
+        if( games.length === 0 ) return true
+
+        if( matchesPerRound === games.length || !match ) return false
+
+        const isInTheRound = games.find( game => {
+            
+            if( game.home.id === match.home.id  || game.visitor.id === match.home.id )
+                return game
+
+            if( game.home.id === match.visitor.id  || game.visitor.id === match.visitor.id )
+                return game
+
+        })
+
+        return isInTheRound ? false : true
 
     }
 
